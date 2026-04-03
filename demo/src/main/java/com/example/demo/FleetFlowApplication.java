2890df2558f0411350aca901848445c2bd0c243f
aymen ELFADILI <aitabbou111@gmail.com>
2026-04-03 15:02:12 +0100

    Refactor: Rename application to FleetFlowApplication, update port and add validation dependency

--- demo/src/main/java/com/example/demo/FleetFlowApplication.java
+++ demo/src/main/java/com/example/demo/FleetFlowApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class FleetFlowApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FleetFlowApplication.class, args);
+    }
 
 }
