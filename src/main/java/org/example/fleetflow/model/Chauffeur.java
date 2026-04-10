32bb876688d8169c9ae1d704adab9ef1f6038f43
Imane Ramadane <ramadaneimane@gmail.com>
2026-04-10 10:54:15 +0100

    commit

--- src/main/java/org/example/fleetflow/model/Chauffeur.java
+++ src/main/java/org/example/fleetflow/model/Chauffeur.java
@@ -1,10 +1,7 @@
 package org.example.fleetflow.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -12,6 +9,7 @@ import lombok.*;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name="chauffeur")
 public class Chauffeur {
 
     @Id
